80d40aee8b42bc0dd87f54290a73155e9e4c2069
MouadSadik <sadikmoad90@gmail.com>
2025-10-30 13:28:17 +0100

    Initial commit

---
+++ src/main/java/com/sadik/jwt_security/config/JwtService.java
@@ -0,0 +1,81 @@
+package com.sadik.jwt_security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+//claims = infos (payload)
+
+@Service
+public class JwtService {
+
+    private final static String SECRET_KEY = "2c5192798c3bd8f26d7d72b90548ea3654f416e4b2dcb01c376f6dafb2c7cb79";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isValidToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isExpiredToken(token);
+    }
+
+    public boolean isExpiredToken(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+    // This method extracts all the claims (data) from a JWT token
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()                 // Create a new JWT parser builder
+                .setSigningKey(getSignInKey())   // Set the secret key used to verify the token's signature
+                .build()                         // Build the JWT parser
+                .parseClaimsJws(token)            // Parse the token (verify signature and decode)
+                .getBody();                       // Return the body (payload) which contains all the claims
+    }
+
+
+    public Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
---
+++ src/main/java/com/sadik/jwt_security/user/Role.java
@@ -0,0 +1,7 @@
+package com.sadik.jwt_security.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
